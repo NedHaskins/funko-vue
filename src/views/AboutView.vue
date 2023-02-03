4d3497347070d0ed91e7ddae2fa8312734dad25e
Ned <ed.hask85@gmail.com>
2023-02-03 16:50:25 -0500

    Added “about” and “contact” detail info

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,36 @@
 <script setup></script>
 
 <template>
-	<h1>About</h1>
+	<inner-column>
+		<module-header>
+			<h2>About This Project</h2>
+		</module-header>
 
-	<!-- insert ArtifactList here, which will grab from the data store -->
+		<p>This site runs on the Node.js runtime environment.</p>
+
+		<p>HTML, CSS, and JavaScript are the languages used in the source code.</p>
+
+		<p>
+			The project is built from scratch with Vue 3, a JavaScript framework / UI library. It allows for application
+			components to be grouped together and partialed out in logical ways, improving developer experience during
+			application design, testing, and delivery.
+		</p>
+
+		<p>
+			The project is a culmination of the work done during the Design for the Web course — and it will continue to be
+			built out over time!
+		</p>
+	</inner-column>
 </template>
+
+<style scoped>
+	inner-column {
+		max-width: 500px;
+	}
+
+	p {
+		font-family: 'Fredoka One';
+		font-size: 24px;
+		margin-top: 30px;
+	}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,38 @@
 <script setup></script>
 
-<template>
-	<h1>Contact us for more info!</h1>
+<script setup></script>
 
-	<!-- insert ArtifactList here, which will grab from the data store -->
+<template>
+	<inner-column>
+		<module-header>
+			<h2>Want to learn more?</h2>
+		</module-header>
+
+		<p>Ned Haskins is a web designer based in Richmond, Virginia.</p>
+
+		<p>He loves to connect with clients and work with them to achieve their goals!</p>
+
+		<p>
+			Email him at
+
+			<a href="mailto:hello@nedhaskins.io">hello@nedhaskins.io</a> to start a conversation!
+		</p></inner-column
+	>
 </template>
+
+<style scoped>
+	inner-column {
+		max-width: 500px;
+	}
+
+	a {
+		color: var(--vividMulberry);
+	}
+	p {
+		font-family: 'Fredoka One';
+		font-size: 24px;
+		margin-top: 30px;
+	}
+</style>
 
 <style scoped></style>
