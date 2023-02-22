bfa76d291e1593c1390361f89c7ecef4ac3d03ce
Ned <ed.hask85@gmail.com>
2023-02-22 10:37:03 -0500

    Fixed logic to account for route value skip during rendering

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
-	import { RouterView, RouterLink } from 'vue-router';
+	import { RouterView, RouterLink, useRoute } from 'vue-router';
 
 	import SiteHeader from '@/components/SiteHeader.vue';
 	import SiteFooter from '@/components/SiteFooter.vue';
 
 	import { onMounted, watch } from 'vue';
+
+	const route = useRoute();
+
+	// function checkRoute() {
+	// 	let name = route.name;
+	// 	if (name.includes('intro')) {
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }
+
+	// checkRoute();
 
 	// onMounted(function () {
 	// 	if (localStorage.getItem('shoppingCartData')) {
@@ -21,13 +34,15 @@
 </script>
 
 <template>
-	<SiteHeader />
+	<pre><code>{{route.name}}</code></pre>
+	<SiteHeader v-if="!route.name?.includes('intro')" />
+	<!-- the question mark here allows the `skip` that happens in the route value during rendering.  This fixed the problem of the pages not loading successfully during this check.-->
 	<main class="outlet">
 		<inner-column>
 			<RouterView />
 		</inner-column>
 	</main>
-	<SiteFooter />
+	<SiteFooter v-if="!route.name?.includes('intro')" />
 </template>
 
 <style scoped></style>
